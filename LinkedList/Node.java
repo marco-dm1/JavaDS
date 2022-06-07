6e0ce9f446469e81568e9da4e9ce9a08d588086d
marco-dm1 <73443992+marco-dm1@users.noreply.github.com>
2022-06-06 23:02:32 -0400

    Create Node.java

---
+++ LinkedList/Node.java
@@ -0,0 +1,14 @@
+/*
+    Marco Diaz Moore © June 2022
+    Linked List implementation
+*/
+
+public class Node {
+    public int value;
+    public Node next;
+
+    public Node(int iValue, Node iNode){
+        this.value = iValue;
+        this.next = iNode;
+    }
+}
